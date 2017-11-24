63be99ca3504aa21559a54721d18fb8aefca1a46
Jón Tryggvi Unnarsson <jontryggviunnarsson@Jons-MacBook-Pro.local>
2017-11-24 10:30:56 +0100

    sass setup

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tech Tornado</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>Tech Tornado is up and running!!</h1>
+  </body>
+</html>
