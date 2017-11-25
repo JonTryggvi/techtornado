238a3950c194654d7e56a6f171a0902ff046726b
Jón Tryggvi Unnarsson <jontryggviunnarsson@Jons-MBP.lan>
2017-11-25 08:25:53 +0100

    testing not concluded

--- index.php
+++ index.php
@@ -7,7 +7,14 @@
 
   </head>
   <body>
-    <?php require_once('partials/svg.php'); ?> 
+    <?php require_once('partials/svg.php'); ?>
+    <div class="size">
+      <svg>
+        <use xlink:href="#svg_drink_red">
+      </svg>
+    </div>
+
+    <!-- <img class="size" src="img/icons/SVG/red_drink.svg" alt=""> -->
     <h1>Tech Tornado is up and running!!</h1>
   </body>
 </html>
