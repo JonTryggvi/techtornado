597eb7b3371e990fc86f7545860857ef977e330e
Jón Tryggvi Unnarsson <jontryggviunnarsson@Jons-MBP.lan>
2017-11-27 19:30:59 +0100

    lets commit

--- js/custom/jt.js
+++ js/custom/jt.js
@@ -1,30 +1,43 @@
-var nextArrow = $('.nextArrow');
-var prevArrow = $('.prevArrow');
-var nextText = $('.nextText');
-var prevText = $('.prevText');
-var headSlider = $('.fp-hero__slider__content');
-nextArrow.hover(
-  function() {
-    prevArrow.css('opacity', 0).css('transition', 'all 100ms ease');
-    nextText.css('width', '90px').css('transition', 'all 300ms ease');
-  },
-  function() {
-    prevArrow.css('opacity', 1).css('transition', 'all 300ms 300ms ease');
-    nextText.css('width', 0).css('transition', 'all 300ms ease');
-  }
-);
-
-prevArrow.hover(
-  function() {
-    nextArrow.css('opacity', 0).css('transition', 'all 100ms ease');
-    prevText.css('width', '120px').css('transition', 'all 300ms ease');
-  },
-  function() {
-    nextArrow.css('opacity', 1).css('transition', 'all 300ms ease');
-    prevText.css('width', 0).css('transition', 'all 300ms ease');
-  });
 $(document).ready(function() {
+  var nextArrow = $('.nextArrow');
+  var prevArrow = $('.prevArrow');
+  var nextText = $('.nextText');
+  var prevText = $('.prevText');
+  var headSlider = $('.owl-carousel');
+  nextArrow.hover(
+    function() {
+      prevArrow.css('opacity', 0).css('transition', 'all 100ms ease');
+      nextText.css('width', '90px').css('transition', 'all 300ms ease');
+    },
+    function() {
+      prevArrow.css('opacity', 1).css('transition', 'all 300ms 100ms ease');
+      nextText.css('width', 0).css('transition', 'all 200ms ease');
+    }
+  );
+
+  prevArrow.hover(
+    function() {
+      nextArrow.css('opacity', 0).css('transition', 'all 100ms ease');
+      prevText.css('width', '120px').css('transition', 'all 300ms ease');
+    },
+    function() {
+      nextArrow.css('opacity', 1).css('transition', 'all 300ms 100ms ease');
+      prevText.css('width', 0).css('transition', 'all 200ms ease');
+    }
+  );
+
   headSlider.owlCarousel({
-    'items': 1
+    'items': 1,
+    'loop': true,
+    'smartSpeed': 500
   });
-})
+
+  nextArrow.click(function() {
+    console.log('y');
+    headSlider.trigger('next.owl.carousel');
+  });
+
+  prevArrow.click(function() {
+    headSlider.trigger('prev.owl.carousel');
+  });
+});
