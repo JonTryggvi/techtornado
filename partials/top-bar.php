c85acf7f050fb9e1b4d46129eeefdaf75adc3b0e
Jón Tryggvi Unnarsson <jontryggviunnarsson@Jons-MacBook-Pro.local>
2017-11-27 15:06:42 +0100

    because i can

---
+++ partials/top-bar.php
@@ -0,0 +1,8 @@
+<div class="top-bar-container">
+  <div class="top-bar wrapper">
+    <svg class="top-bar__logo">
+      <use href="#topBarLogo"></use>
+    </svg>
+    <a class="top-bar__sign-in" href="#">Sign In</a>
+  </div>
+</div>
