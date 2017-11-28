431b31f0ce9f9ad2e8d4f05ab77cb5bf13e75bfa
Katrin Sigurdardottir <katrinduasig@gmail.com>
2017-11-28 14:27:12 +0100

    signIn icon

--- partials/signIn.php
+++ partials/signIn.php
@@ -10,25 +10,34 @@
               <g id="Page-1" transform="translate(8.000000, 109.000000)">
                 <g id="Group-51" transform="translate(156.000000, 501.000000)">
                   <polygon id="Fill-20" class="envelopeRed" fill="#FF6565" points="65.2889378 1.5518971 36.7362584 29.1819441 7.29130767 1.5518971"></polygon>
-                  <polygon id="Stroke-21" stroke="#AFC5DB" stroke-width="3" points="0.362309589 39.1087358 58.3599397 39.1087358 58.3599397 0.783186722 0.362309589 0.783186722"></polygon>
-                  <polyline id="Stroke-22" stroke="#AFC5DB" stroke-width="3" points="0.362155616 0.782879115 29.8071063 28.4129261 58.3597858 0.782879115"></polyline>
-                  <path d="M20.7837729,19.9458075 L0.362386575,39.1089665" id="Stroke-23" stroke="#AFC5DB" stroke-width="3"></path>
-                  <path d="M38.5573693,19.9458075 L58.3597858,39.1089665" id="Stroke-24" stroke="#AFC5DB" stroke-width="3"></path>
+                  <polygon id="Stroke-21" class="envelopeRedToBlack" stroke="#AFC5DB" stroke-width="3" points="0.362309589 39.1087358 58.3599397 39.1087358 58.3599397 0.783186722 0.362309589 0.783186722"></polygon>
+                  <polyline id="Stroke-22" class="envelopeRedToBlack" stroke="#AFC5DB" stroke-width="3" points="0.362155616 0.782879115 29.8071063 28.4129261 58.3597858 0.782879115"></polyline>
+                  <path d="M20.7837729,19.9458075 L0.362386575,39.1089665" class="envelopeRedToBlack" id="Stroke-23" stroke="#AFC5DB" stroke-width="3"></path>
+                  <path d="M38.5573693,19.9458075 L58.3597858,39.1089665" class="envelopeRedToBlack" id="Stroke-24" stroke="#AFC5DB" stroke-width="3"></path>
                 </g>
               </g>
             </g>
           </g>
         </svg>
-        <!-- <svg class="logIn_email">
-          <use href="#envelope"></use>
-        </svg> -->
-        <input type="text" name="logIn_email" value="" placeholder="E-mail address">
+        <input type="text" name="logIn_email" class="inputLogInEmail" value="" placeholder="E-mail address">
       </label>
       <label for="logIn_password">
-        <svg class="logIn_password">
-          <use href="#lock"></use>
+        <svg id="lockRed" class="logIn_password" viewBox="0 0 54 58" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+          <g id="Style-Guide" transform="translate(-622.000000, -2447.000000)">
+            <g id="Group-14" transform="translate(119.000000, 2019.000000)">
+              <g id="Page-1" transform="translate(8.000000, 109.000000)">
+                <g id="Group-42" transform="translate(497.000000, 321.000000)">
+                  <path d="M16.3164556,37.288738 C16.3164556,31.9794379 20.624609,27.6760133 25.9397433,27.6760133 C31.2548775,27.6760133 35.563031,31.9794379 35.563031,37.288738 C35.563031,42.5980382 31.2548775,46.9014628 25.9397433,46.9014628 C20.624609,46.9014628 16.3164556,42.5980382 16.3164556,37.288738" id="Fill-61" class="lockRed" fill="#FF6565"></path>
+                  <path d="M17.4712501,21.5238694 L17.4712501,12.2956537 C17.4712501,5.95125533 22.6678255,0.760383956 29.0191953,0.760383956 C35.3705652,0.760383956 40.5671405,5.95125533 40.5671405,12.2956537 L40.5671405,21.5238694 L17.4712501,21.5238694 Z" id="Fill-62" class="lockRed" fill="#FF6565"></path>
+                  <path d="M45.3954134,54.3993881 L5.3294326,54.3993881 C2.58641068,54.3993881 0.341490137,52.1577007 0.341490137,49.4176896 L0.341490137,26.3133134 C0.341490137,23.5733024 2.58641068,21.3316149 5.3294326,21.3316149 L45.3954134,21.3316149 C48.1384353,21.3316149 50.382586,23.5733024 50.382586,26.3133134 L50.382586,49.4176896 C50.382586,52.1577007 48.1384353,54.3993881 45.3954134,54.3993881 Z" id="Stroke-63" class="lockRedBlack" stroke="#AFC5DB" stroke-width="3"></path>
+                  <path d="M13.6219351,21.1393604 L13.6219351,11.9111447 C13.6219351,5.56674633 18.8185104,0.375874965 25.1698803,0.375874965 C31.5212501,0.375874965 36.7178255,5.56674633 36.7178255,11.9111447 L36.7178255,21.1393604" id="Stroke-64" class="lockRedBlack" stroke="#AFC5DB" stroke-width="3"></path>
+                  <path d="M19.2034419,37.8655015 C19.2034419,34.4679801 21.9610912,31.7133577 25.362346,31.7133577 C28.7636008,31.7133577 31.5212501,34.4679801 31.5212501,37.8655015 C31.5212501,41.263023 28.7636008,44.0176454 25.362346,44.0176454 C21.9610912,44.0176454 19.2034419,41.263023 19.2034419,37.8655015 Z" id="Stroke-65" class="lockRedBlack" stroke="#AFC5DB" stroke-width="3"></path>
+                </g>
+              </g>
+            </g>
+          </g>
         </svg>
-        <input type="text" name="logIn_password" value="" placeholder="Password">
+        <input type="text" name="logIn_password" class="inputLogInPassword" value="" placeholder="Password">
       </label>
       <a href="#">No account yet? Create one</a>
       <button type="button" name="button">Sign In</button>
@@ -36,14 +45,14 @@
     <div class="signIn__logInForm__withMedia">
       <a href="" class="withFacebook">Sign in with Facebook
         <svg id="arrowForwRed" class="logIn_facebook" viewBox="0 0 55 41" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" >
-          <polygon id="Fill-41" class="arrowForwRed" fill="#FF6565" points="29.3808877 0.727521992 48.0639233 19.3900503 29.3808877 38.0518097"></polygon>
+          <polygon id="Fill-41" class="arrowForwRedFacebook" fill="#FF6565" points="29.3808877 0.727521992 48.0639233 19.3900503 29.3808877 38.0518097"></polygon>
           <path d="M0.31740411,19.3897427 L52.499489,19.3897427" id="Stroke-42" stroke="#3E3E3E" stroke-width="3"></path>
           <polyline id="Stroke-43" stroke="#3E3E3E" stroke-width="3" points="34.0046079 0.727521992 52.6876436 19.3900503 34.0046079 38.0518097"></polyline>
         </svg>
       </a>
       <a href="" class="withGoogle">Sign in with Google
         <svg id="arrowForwRed" class="logIn_google" viewBox="0 0 55 41" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" >
-          <polygon id="Fill-41" class="arrowForwRed" fill="#FF6565" points="29.3808877 0.727521992 48.0639233 19.3900503 29.3808877 38.0518097"></polygon>
+          <polygon id="Fill-41" class="arrowForwRedGoogle" fill="#FF6565" points="29.3808877 0.727521992 48.0639233 19.3900503 29.3808877 38.0518097"></polygon>
           <path d="M0.31740411,19.3897427 L52.499489,19.3897427" id="Stroke-42" stroke="#3E3E3E" stroke-width="3"></path>
           <polyline id="Stroke-43" stroke="#3E3E3E" stroke-width="3" points="34.0046079 0.727521992 52.6876436 19.3900503 34.0046079 38.0518097"></polyline>
         </svg>
