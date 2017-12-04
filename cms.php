7201e9c1e314dd09ea9e9d935402e9e0e39f6705
Jón Tryggvi Unnarsson <jontryggviunnarsson@laptop-b3gtgdh2.kea.local>
2017-12-04 12:34:05 +0100

    structure on its' way

--- cms.php
+++ cms.php
@@ -16,7 +16,13 @@
     <?php include_once('partials/svg.php'); ?>
     <?php include_once('partials/top-bar.php'); ?>
     <?php include_once('partials/hero.php'); ?>
+    <?php include_once('partials/cms-view.php');?>
     <?php include_once('partials/signin.php'); ?>
+    <?php if ($isLoggedin) {
+     include_once('partials/cms-view.php');
+ } ?>
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.2.1/owl.carousel.min.js"></script>
     <script type="text/javascript" src="js/scripts.js"></script>
