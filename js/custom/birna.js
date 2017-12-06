2d519b8ad9019aa8c33dfcf2be226db914f0cc17
Jón Tryggvi Unnarsson <jontryggviunnarsson@erling-pc.kea.local>
2017-12-06 11:47:38 +0100

    birnajs fix

--- js/custom/birna.js
+++ js/custom/birna.js
@@ -41,53 +41,7 @@ $(document).ready(function() {
     }
   });
 
-  //image slider
-  $("#myImageSlider").owlCarousel({
-    stagePadding: 100,
-    loop: true,
-    margin: 10,
-    nav: false,
-    items: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    autoplayTimeout: 10000,
-    responsive: {
-      600: {
-        items: 1,
-        stagePadding: 60
-      },
-      1000: {
-        items: 1,
-        stagePadding: 100
-      },
-      1400: {
-        items: 1,
-        stagePadding: 140
-      },
-      1600: {
-        items: 1,
-        stagePadding: 350
-      },
-      1800: {
-        items: 1,
-        stagePadding: 400
-      }
-    }
-  });
 
-
-  // open the sponsers modal
-  var btnShowSponsers = $('#btnShowSponsers');
-  var btnCLoseSponsers = $('#btnCLoseSponsers');
-  var sponsorsModal = $('#sponsorsModal');
-
-  <<
-  << << < HEAD
-  btnShowSponsers.click(function() {
-    console.log('x');
-    sponsorsModal.addClass('scaleUp');
-  }); ===
-  === =
 
   //to trigger next slide by clicking on item in slide.
   //butt how to trigger next and previous??
@@ -109,61 +63,16 @@ $(document).ready(function() {
   // open the sponsers modal
   var btnShowSponsers = $('#btnShowSponsers');
   var btnCLoseSponsers = $('#btnCLoseSponsers');
-  var sponsorsModal = $('#sponsorsModal'); >>>
-  >>> > devMaster
+  var sponsorsModal = $('#sponsorsModal');
+
+  btnShowSponsers.click(function() {
+    console.log('x');
+    sponsorsModal.addClass('scaleUp');
+  });
 
   btnCLoseSponsers.click(function() {
     console.log('x');
     sponsorsModal.removeClass('scaleUp');
   });
 
-  <<
-  << << < HEAD
-
-
-  //image slider
-  $("#myImageSlider").owlCarousel({
-    stagePadding: 100,
-    loop: true,
-    margin: 10,
-    nav: false,
-    items: 1,
-    autoplay: true,
-    autoplaySpeed: 1500,
-    autoplayTimeout: 10000,
-    responsive: {
-      600: {
-        items: 1,
-        stagePadding: 60
-      },
-      1000: {
-        items: 1,
-        stagePadding: 100
-      },
-      1400: {
-        items: 1,
-        stagePadding: 140
-      },
-      1600: {
-        items: 1,
-        stagePadding: 350
-      },
-      1800: {
-        items: 1,
-        stagePadding: 400
-      }
-    }
-
-
-  });
-
-
-}); ===
-=== =
-btnCLoseSponsers.click(function() {
-console.log('x');
-sponsorsModal.removeClass('scaleUp');
 });
-
-}); >>>
->>> > devMaster
