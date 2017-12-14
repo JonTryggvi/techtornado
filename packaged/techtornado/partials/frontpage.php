fdec25d1c8802b365903f8badc9813c846608c08
Birna Bryndís <thorkelsdottir@gmail.com>
2017-12-14 17:34:30 +0100

    jajaja

--- packaged/techtornado/partials/frontpage.php
+++
@@ -1,5 +0,0 @@
-  <?php include_once('partials/hero.php'); ?>
-  <?php include_once('partials/createevent.php'); ?>
-  <?php include_once('partials/imageslider.php'); ?>
-  <?php include_once('partials/subscribe.php'); ?>
-  <?php include_once('partials/footer.php'); ?>
--- packaged/techtornado/partials/loader.php
+++
@@ -1,9 +0,0 @@
-<div class="loader">
-  <div class="myTornado">
-    <span class="firstLine"></span>
-    <span class="secondLine"></span>
-    <span class="thirdLine"></span>
-    <span class="fourthLine"></span>
-    <span class="fifthLine"></span>
-  </div>
-</div>
